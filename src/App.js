b9c140e1a8830c601ce0c5e1267946b664e0222d
Debashish D. Dev <hello@debashish.dev>
2022-05-03 19:51:26 +0600

    Added Footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import Footer from "./Components/Shared/Footer";
 import Header from "./Components/Shared/Header";
 
 function App() {
   return (
-    <div>
+    <div className="relative">
       <Header></Header>
       <h1 className="text-3xl font-bold text-center">Hello world!</h1>
+      <Footer></Footer>
     </div>
   );
 }
