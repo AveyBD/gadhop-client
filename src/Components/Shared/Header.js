96fe82917969760f18bcace156945f1e93157dc3
Debashish D. Dev <hello@debashish.dev>
2022-05-05 17:46:01 +0600

    added green color as primary

--- src/Components/Shared/Header.js
+++ src/Components/Shared/Header.js
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
 import { Transition } from "@headlessui/react";
 import { Link } from "react-router-dom";
-import logo  from "../../assets/images/logo.png"
+import logo from "../../assets/images/logo.png";
 import Hero from "../Hero/Hero";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserAlt, faUserAltSlash } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isUserOpen, setIsUserOpen] = useState(false);
+
   return (
     <div>
       <nav className="bg-white border-b-2">
@@ -104,7 +108,23 @@ const Header = () => {
             </div>
           </div>
           <div>
-            <h1 className="order-first md:order-last">Hello</h1>
+            <h1 className="order-first md:order-last">
+              {/* nav start  */}
+              <button
+                onClick={() => setIsUserOpen(!isUserOpen)}
+                type="button"
+                className="bg-white border rounded-full inline-flex items-center justify-center p-2 rounded-md text-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-300 focus:ring-white"
+                aria-controls="profile-user-menu"
+                aria-expanded="false"
+              >
+                <span className="sr-only">Open Profile menu</span>
+                {!isOpen ? (
+                 <FontAwesomeIcon icon={faUserAlt}></FontAwesomeIcon>
+                ) : ( <FontAwesomeIcon icon={faUserAltSlash}></FontAwesomeIcon>
+                )}
+              </button>
+              {/* nav end  */}
+            </h1>
           </div>
         </div>
 
@@ -158,6 +178,59 @@ const Header = () => {
             </div>
           )}
         </Transition>
+
+        {/* profile menu  */}
+        <Transition
+          show={isUserOpen}
+          enter="transition ease-out duration-100 transform"
+          enterFrom="opacity-0 scale-95"
+          enterTo="opacity-100 scale-100"
+          leave="transition ease-in duration-75 transform"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-95"
+        >
+          {(ref) => (
+            <div className="md:w-60 md:ml-auto" id="profile-user-menu">
+              <div ref={ref} className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+                <Link
+                  to="/login"
+                  className="hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Login
+                </Link>
+
+                <Link
+                  to="/register"
+                  className="hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Register
+                </Link>
+
+                <Link
+                  to="/"
+                  className="hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  My Items
+                </Link>
+
+                <Link
+                  to="/"
+                  className="hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Add Items
+                </Link>
+
+                <Link
+                  to="/"
+                  className="hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Signout
+                </Link>
+              </div>
+            </div>
+          )}
+        </Transition>
+        {/* profile menu end  */}
       </nav>
     </div>
   );
