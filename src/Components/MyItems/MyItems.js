cfba763abf54cddf4b0fcb5c2997f2750044e9c2
Debashish D. Dev <hello@debashish.dev>
2022-05-08 21:48:56 +0600

    UID Done

--- src/Components/MyItems/MyItems.js
+++ src/Components/MyItems/MyItems.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const MyItems = () => {
     const [myProducts, setMyProducts] = useState([]);
+    const [user] = useAuthState(auth);
 
     useEffect(() => {
-      fetch("https://gadhop.herokuapp.com/my")
+      fetch(`https://gadhop.herokuapp.com/my?uid=${user.uid}`)
         .then((res) => res.json())
         .then((data) => setMyProducts(data));
     }, []);
