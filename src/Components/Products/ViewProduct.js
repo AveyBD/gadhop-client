3a3acb5e94f9feb28c818a256f4f390ec0be4634
Debashish D. Dev <hello@debashish.dev>
2022-05-08 22:46:17 +0600

    Showing Single Product

--- src/Components/Products/ViewProduct.js
+++ src/Components/Products/ViewProduct.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const ViewProduct = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const [sProduct, setSProduct] = useState({});
+  const { id } = useParams();
+  useEffect(() => {
+    const url = `https://gadhop.herokuapp.com/view/${id}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setSProduct(data));
+  }, []);
+  console.log(sProduct);
+  return (
+    <div>
+      <h2>{sProduct.name}</h2>
+    </div>
+  );
 };
 
 export default ViewProduct;
