f45acf74ae91d89c43725a332063dc4e59fae6f7
Debashish D. Dev <hello@debashish.dev>
2022-05-08 17:37:14 +0600

    Manage Item table

--- src/Components/Manage/Manage.js
+++ src/Components/Manage/Manage.js
@@ -10,11 +10,54 @@ const Manage = () => {
       .then((data) => setProducts(data));
   }, []);
   return (
-    <div>
-      <h2>Manage Here {products.length}
-      {
-          console.log(products)
-      }</h2>
+    <div className="md:w-3/4 mx-auto mt-6">
+      <h2 className="text-3xl font-bold text-center mb-4">
+        Manage All Product
+      </h2>
+      <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" class="px-6 py-3">
+                Product name
+              </th>
+              <th scope="col" class="px-6 py-3">
+                Image
+              </th>
+              <th scope="col" class="px-6 py-3">
+                Supplier
+              </th>
+              <th scope="col" class="px-6 py-3">
+                Price
+              </th>
+              <th scope="col" class="px-6 py-3">
+                <span class="sr-only">View</span>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr class="border-b dark:bg-gray-800 dark:border-gray-700 odd:bg-white even:bg-gray-50 odd:dark:bg-gray-800 even:dark:bg-gray-700">
+              <th
+                scope="row"
+                class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap"
+              >
+                Apple MacBook Pro 17"
+              </th>
+              <td class="px-6 py-4">Sliver</td>
+              <td class="px-6 py-4">Laptop</td>
+              <td class="px-6 py-4">$2999</td>
+              <td class="px-6 py-4 text-right">
+                <a
+                  href="#"
+                  class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                >
+                  Edit
+                </a>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
