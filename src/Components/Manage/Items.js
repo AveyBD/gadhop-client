d1b62d07eff43a9b39e4e301a785832c7896ab27
Debashish D. Dev <hello@debashish.dev>
2022-05-08 17:56:56 +0600

    Inventory Loading

--- src/Components/Manage/Items.js
+++ src/Components/Manage/Items.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React from "react";
 
-const Items = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const Items = ({product}) => {
+  return (
+    <tr className="border-b dark:bg-gray-800 dark:border-gray-700 odd:bg-white even:bg-gray-50 odd:dark:bg-gray-800 even:dark:bg-gray-700">
+      <td
+        scope="row"
+        className="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap"
+      >
+        {product.name}
+      </td>
+      <td className="px-6 py-4">{product.supplier}</td>
+      <td className="px-6 py-4">{product.supplier}</td>
+      <td className="px-6 py-4">{product.price}</td>
+      <td className="px-6 py-4 text-right">
+        <a
+          href="#"
+          className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+        >
+          View
+        </a>
+      </td>
+    </tr>
+  );
 };
 
 export default Items;
--- src/Components/Manage/Manage.js
+++ src/Components/Manage/Manage.js
@@ -1,5 +1,6 @@
 import { data } from "autoprefixer";
 import React, { useEffect, useState } from "react";
+import Items from "./Items";
 
 const Manage = () => {
   const [products, setProducts] = useState([]);
@@ -9,52 +10,35 @@ const Manage = () => {
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
+
   return (
     <div className="md:w-3/4 mx-auto mt-6">
       <h2 className="text-3xl font-bold text-center mb-4">
         Manage All Product
       </h2>
-      <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-          <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+      <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
-              <th scope="col" class="px-6 py-3">
+              <th scope="col" className="px-6 py-3">
                 Product name
               </th>
-              <th scope="col" class="px-6 py-3">
+              <th scope="col" className="px-6 py-3">
                 Image
               </th>
-              <th scope="col" class="px-6 py-3">
+              <th scope="col" className="px-6 py-3">
                 Supplier
               </th>
-              <th scope="col" class="px-6 py-3">
+              <th scope="col" className="px-6 py-3">
                 Price
               </th>
-              <th scope="col" class="px-6 py-3">
-                <span class="sr-only">View</span>
+              <th scope="col" className="px-6 py-3">
+                <span className="sr-only">View</span>
               </th>
             </tr>
           </thead>
           <tbody>
-            <tr class="border-b dark:bg-gray-800 dark:border-gray-700 odd:bg-white even:bg-gray-50 odd:dark:bg-gray-800 even:dark:bg-gray-700">
-              <th
-                scope="row"
-                class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap"
-              >
-                Apple MacBook Pro 17"
-              </th>
-              <td class="px-6 py-4">Sliver</td>
-              <td class="px-6 py-4">Laptop</td>
-              <td class="px-6 py-4">$2999</td>
-              <td class="px-6 py-4 text-right">
-                <a
-                  href="#"
-                  class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                >
-                  Edit
-                </a>
-              </td>
-            </tr>
+            {products.map((product) =><Items key={product._id} product={product}></Items>)}
           </tbody>
         </table>
       </div>
