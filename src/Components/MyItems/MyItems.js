8b56c960c5f218ecb4fc86f191c28340ca7374da
Debashish D. Dev <hello@debashish.dev>
2022-05-08 18:15:48 +0600

    loading my data

--- src/Components/MyItems/MyItems.js
+++ src/Components/MyItems/MyItems.js
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const MyItems = () => {
+    const [myProducts, setMyProducts] = useState([]);
+
+    useEffect(() => {
+      fetch("https://gadhop.herokuapp.com/my")
+        .then((res) => res.json())
+        .then((data) => setMyProducts(data));
+    }, []);
     return (
         <div>
             <h2>User Added Items here </h2>
