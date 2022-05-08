ac54a6df30670435b89beb2c17a7ea9149a11152
Debashish D. Dev <hello@debashish.dev>
2022-05-08 15:10:20 +0600

    Fixed Reload to Login

--- src/Components/Shared/RequiredAuth.js
+++ src/Components/Shared/RequiredAuth.js
@@ -2,10 +2,14 @@ import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { Navigate, useLocation } from "react-router-dom";
 import auth from "../../firebase.init";
+import Loader from "./Loader";
 
 const RequiredAuth = ({ children }) => {
-  const [user] = useAuthState(auth);
+  const [user, loading] = useAuthState(auth);
   const location = useLocation();
+  if (loading) {
+    return <Loader></Loader>;
+  }
   if (!user) {
     return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
   }
