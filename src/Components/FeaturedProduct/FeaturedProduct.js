62e31ad96d0e688157606c548912f7b67907c982
Debashish D. Dev <hello@debashish.dev>
2022-05-09 17:35:29 +0600

    A Manage Inventory Button bellow featured product

--- src/Components/FeaturedProduct/FeaturedProduct.js
+++ src/Components/FeaturedProduct/FeaturedProduct.js
@@ -1,15 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import useProducts from "../../hooks/useProducts";
 import SingleFeatured from "./SingleFeatured";
 
 const FeaturedProduct = () => {
   const [products, setProducts] = useProducts();
   return (
+      <>
     <div className="md:w-3/4 grid grid-cols-1 md:grid-cols-6 gap-1 mx-auto">
-      {products.slice(0,6).map((product) => (
+      {products.slice(0, 6).map((product) => (
         <SingleFeatured key={product._id} product={product}></SingleFeatured>
       ))}
     </div>
+    <Link className="block w-1/2 py-2 rounded text-center font-bold mt-3 bg-green-400 mx-auto " to={"/manage"}>
+        Manage Inventories
+      </Link>
+    </>
   );
 };
 
