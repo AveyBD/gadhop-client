d3746b73090dd535b4bd21a2552c0d42ae62a10e
Debashish D. Dev <hello@debashish.dev>
2022-05-09 13:13:33 +0600

    Showing Params

--- src/Components/Products/UpdateProduct.js
+++ src/Components/Products/UpdateProduct.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const UpdateProduct = () => {
+    const {id} = useParams();
     return (
         <div>
-            <h2>Update Your Product here!</h2>
+            <h2>Update Your Product here!{id}</h2>
         </div>
     );
 };
